49b4b43a9337a1cd3aac71b3d6b764d4ac516779
Shimady563 <shimady563@gmail.com>
2025-02-19 19:06:50 +0300

    build: turn off kafka and liquibase for now

--- compiler/src/test/java/com/shimady/contest/compiler/TestCompilerApplication.java
+++ compiler/src/test/java/com/shimady/contest/compiler/TestCompilerApplication.java
@@ -2,7 +2,7 @@ package com.shimady.contest.compiler;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestCompilatorApplication {
+public class TestCompilerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(CompilerApplication::main).with(TestcontainersConfiguration.class).run(args);
