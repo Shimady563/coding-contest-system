c09e16d3abe34f6f798ec33695708ec1c16d309c
Shimady563 <shimady563@gmail.com>
2025-02-28 17:17:31 +0300

    feat: add cors configuration

--- compiler/src/main/java/com/shimady/contest/compiler/config/GenericConfig.java
+++ compiler/src/main/java/com/shimady/contest/compiler/config/GenericConfig.java
@@ -3,10 +3,27 @@ package com.shimady.contest.compiler.config;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Configuration
 public class GenericConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
