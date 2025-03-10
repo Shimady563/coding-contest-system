b90ef1a27bd6cd47c4192a16e43fc7c21291c21a
Maksim Shiryaev <shimady563@gmail.com>
2025-03-10 13:32:46 +0300

    fix: fix solution status field problem

--- compiler/src/main/java/com/shimady/contest/compiler/model/Solution.java
+++ compiler/src/main/java/com/shimady/contest/compiler/model/Solution.java
@@ -19,6 +19,7 @@ public class Solution {
     private String code;
 
     @Column(name = "status")
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Column(name = "tests_passed")
