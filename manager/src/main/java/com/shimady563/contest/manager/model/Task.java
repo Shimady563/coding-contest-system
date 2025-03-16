644ebec51e06e9d8e571b950d45b2b7a7ebf453f
Maksim Shiryaev <shimady563@gmail.com>
2025-03-16 19:00:35 +0300

    feat(model): create basic models

---
+++ manager/src/main/java/com/shimady563/contest/manager/model/Task.java
@@ -0,0 +1,61 @@
+package com.shimady563.contest.manager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_id_seq"
+    )
+    @SequenceGenerator(
+            name = "task_id_seq",
+            sequenceName = "task_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "test_cases_count", nullable = false)
+    private Short testCasesCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "contest_version_id", nullable = false)
+    private ContestVersion contestVersion;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "task")
+    private List<Solution> solutions = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "task")
+    private Set<TestCase> testCases = new HashSet<>();
+
+    public void addSolution(Solution solution) {
+        solutions.add(solution);
+        solution.setTask(this);
+    }
+
+    public void addTestCase(TestCase testCase) {
+        testCases.add(testCase);
+        testCase.setTask(this);
+    }
+
+    public void removeTestCase(TestCase testCase) {
+        testCases.remove(testCase);
+    }
+}
---
+++ manager/src/main/java/com/shimady563/contest/manager/model/User.java
@@ -0,0 +1,73 @@
+package com.shimady563.contest.manager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "contest_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_id_seq"
+    )
+    @SequenceGenerator(
+            name = "user_id_seq",
+            sequenceName = "user_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_contest_version",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "contest_version_id")
+    )
+    private Set<ContestVersion> contestVersions = new HashSet<>();
+
+    public boolean containsContestVersion(ContestVersion contestVersion) {
+        return contestVersions.contains(contestVersion);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
+
