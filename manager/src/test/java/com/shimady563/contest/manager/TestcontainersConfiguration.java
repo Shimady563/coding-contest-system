87274f5e155a6ffc349726dd59a727f34b4d3399
Shimady563 <shimady563@gmail.com>
2025-03-16 20:15:06 +0300

    fix: fix issue with kafka testcontainers container

--- manager/src/test/java/com/shimady563/contest/manager/TestcontainersConfiguration.java
+++ manager/src/test/java/com/shimady563/contest/manager/TestcontainersConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,9 +12,8 @@ class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    ConfluentKafkaContainer kafkaContainer() {
-        return new ConfluentKafkaContainer("confluentinc/cp-kafka:latest");
-        //return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1"));
     }
 
     @Bean
