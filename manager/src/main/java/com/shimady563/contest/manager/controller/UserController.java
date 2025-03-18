5d738096713476fb198bde24483060468f8bbb6b
Maksim Shiryaev <shimady563@gmail.com>
2025-03-18 14:59:25 +0300

    feat(validation): implement dto validation and validation exception handling

--- manager/src/main/java/com/shimady563/contest/manager/controller/UserController.java
+++ manager/src/main/java/com/shimady563/contest/manager/controller/UserController.java
@@ -4,9 +4,11 @@ import com.shimady563.contest.manager.model.Role;
 import com.shimady563.contest.manager.model.dto.UserResponse;
 import com.shimady563.contest.manager.model.dto.UserUpdateRequest;
 import com.shimady563.contest.manager.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,11 +38,13 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public void updateUserById(@PathVariable Long id, @RequestBody UserUpdateRequest request) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateUserById(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest request) {
         userService.updateUserById(id, request);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUserById(@PathVariable Long id) {
         userService.deleteUserById(id);
     }
