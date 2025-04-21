ffa6fd629767b77a2fdf4d99b63d652189f94b91
Shimady563 <shimady563@gmail.com>
2025-04-21 18:38:02 +0300

    refactor: allow access to groups endpoint

--- manager/src/main/java/com/shimady563/contest/manager/controller/GroupController.java
+++ manager/src/main/java/com/shimady563/contest/manager/controller/GroupController.java
@@ -3,7 +3,6 @@ package com.shimady563.contest.manager.controller;
 import com.shimady563.contest.manager.model.dto.GroupResponseDto;
 import com.shimady563.contest.manager.service.GroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/groups")
 @RequiredArgsConstructor
-@Secured({"ROLE_TEACHER", "ROLE_STUDENT"})
 public class GroupController {
     private final GroupService groupService;
 
