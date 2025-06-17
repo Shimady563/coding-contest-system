6ce9246472389cb5ae015bd47de5716352d050fd
Shimady563 <shimady563@gmail.com>
2025-06-17 18:37:00 +0300

    test(manager): rework e2e tests

--- manager/src/test/java/com/shimady563/contest/manager/endtoend/EndToEndTests.java
+++ manager/src/test/java/com/shimady563/contest/manager/endtoend/EndToEndTests.java
@@ -5,33 +5,48 @@ import com.shimady563.contest.manager.TestcontainersConfiguration;
 import com.shimady563.contest.manager.model.User;
 import com.shimady563.contest.manager.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.http.Method;
+import io.restassured.parsing.Parser;
+import io.restassured.specification.RequestSpecification;
+import lombok.Builder;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+@Slf4j
 @Testcontainers
 @Import(TestcontainersConfiguration.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -42,13 +57,8 @@ public class EndToEndTests {
     @Value("${server.servlet.context-path}")
     private String contextPath;
 
-    @Value("${jwt.token.access.expiration}")
-    private Long accessTokenExpiration;
-
-    private Key accessSecret;
-
-    @Autowired
-    private UserRepository userRepository;
+    @Value("${jwt.token.access.cookie.name}")
+    private String tokenCookieName;
 
     @LocalServerPort
     private Integer port;
@@ -56,106 +66,147 @@ public class EndToEndTests {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     private String teacherToken;
     private String studentToken;
 
     @BeforeAll
-    public void setUpClass(
-            @Value("${jwt.token.access.secret}") String accessSecret) {
-        this.accessSecret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(accessSecret));
-        this.teacherToken = generateAccessToken(userRepository.findById(1L).orElseThrow());
-        this.studentToken = generateAccessToken(userRepository.findById(2L).orElseThrow());
+    void setUpClass(@Value("${jwt.token.access.secret}") String secret,
+                    @Value("${jwt.token.access.expiration}") Long tokenExpiration,
+                    @Autowired UserRepository userRepository) {
+        Key secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        log.info(String.valueOf(userRepository.findById(1L).get().getRole()));
+        log.info(String.valueOf(userRepository.findById(2L).get().getRole()));
+        this.teacherToken = generateAccessToken(userRepository.findById(1L).orElseThrow(), secretKey, tokenExpiration);
+        this.studentToken = generateAccessToken(userRepository.findById(2L).orElseThrow(), secretKey, tokenExpiration);
     }
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         RestAssured.port = port;
         RestAssured.basePath = contextPath;
+        RestAssured.defaultParser = Parser.JSON;
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Позитивные кейсы")
     @MethodSource("getPositiveTestCases")
-    void runPositiveEndToEndTests(EndToEndTestCase testCase) throws Exception {
-        sendRequest(testCase);
+    void runPositiveEndToEndTests(EndToEndTestCase testCase) {
+        sendRequest(preparePositiveTestCase(testCase));
     }
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "Негативные кейсы")
     @MethodSource("getNegativeTestCases")
-    void runNegativeEndToEndTests(EndToEndTestCase testCase) throws Exception {
-        sendRequest(testCase);
+    void runNegativeEndToEndTests(EndToEndTestCase testCase) {
+        sendRequest(prepareNegativeTestCase(testCase));
     }
 
-    private void sendRequest(EndToEndTestCase testCase) throws Exception {
-        String response = given()
-                .headers(testCase.headers())
+    @SneakyThrows
+    private void sendRequest(EndToEndTestCase testCase) {
+        String response = withOptionalBody(given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
                 .queryParams(testCase.queryParams())
-                .cookies(testCase.cookies())
-                .body(testCase.requestBody())
+                .cookies(testCase.cookies()), testCase.requestBody)
                 .when()
                 .request(testCase.method(), testCase.path())
                 .then()
                 .statusCode(testCase.statusCode())
                 .extract()
                 .asString();
-        assertEquals(mapper.readTree(testCase.responseBody), mapper.readTree(response));
+        log.info(testCase.responseBody);
+        if (StringUtils.hasText(response)) {
+            JSONAssert.assertEquals(testCase.responseBody, response, JSONCompareMode.STRICT);
+        }
     }
 
-    private Stream<EndToEndTestCase> getPositiveTestCases() {
-        return Stream.of(
-                new EndToEndTestCase(
-                        Map.of(
-                                "Authorization", "Bearer " + teacherToken
-                        ),
-                        Map.of(
-                                "name", ""
-                        ),
-                        Map.of(),
-                        "",
-                        Method.GET,
-                        "/contests",
-                        200,
-                        """
-                                {"content":[{"id":1,"name":"Контрольная работа по C++: Основы","description":"Контрольная работа по базовому синтаксису C++.","startTime":"2025-04-01T10:00:00","endTime":"2025-04-01T12:00:00","groupId":1},{"id":2,"name":"Контрольная работа по C++: STL","description":"Контрольная работа по библиотеке стандартных шаблонов C++.","startTime":"2025-04-10T10:00:00","endTime":"2025-04-10T12:00:00","groupId":2},{"id":3,"name":"Контрольная работа по C++: ООП","description":"Контрольная работа по объектно-ориентированному программированию в C++.","startTime":"2025-04-20T10:00:00","endTime":"2025-04-20T12:00:00","groupId":3}],"page":{"size":10,"number":0,"totalElements":3,"totalPages":1}}
-                                """
-
-                )
-        );
+    private RequestSpecification withOptionalBody(RequestSpecification spec, String body) {
+        return StringUtils.hasText(body) ? spec.body(body) : spec;
     }
 
-    private Stream<EndToEndTestCase> getNegativeTestCases() {
-        return Stream.of(
-                new EndToEndTestCase(
-                        Map.of(
-                                "Authorization", "Bearer " + studentToken
-                        ),
-                        Map.of(
-                                "name", ""
-                        ),
-                        Map.of(),
-                        "",
-                        Method.GET,
-                        "/contests",
-                        403,
-                        ""
-                )
-        );
+    private EndToEndTestCase prepareTestCase(EndToEndTestCase testCase, String filePrefix) {
+        String fullPath = filePrefix + testCase.path + testCase.filePathPostfix;
+        return testCase.toBuilder()
+                .queryParams(loadToMap(fullPath + "/queryParams.json"))
+                .requestBody(loadRaw(fullPath + "/requestBody.json"))
+                .responseBody(loadRaw(fullPath + "/responseBody.json"))
+                .build();
     }
 
-    public String generateAccessToken(User user) {
+    private EndToEndTestCase prepareNegativeTestCase(EndToEndTestCase testCase) {
+        return prepareTestCase(testCase, "/negative");
+    }
+
+    @SneakyThrows
+    private EndToEndTestCase preparePositiveTestCase(EndToEndTestCase testCase) {
+        return prepareTestCase(testCase, "/positive");
+    }
+
+    @SneakyThrows
+    private Map<String, Object> loadToMap(String path) {
+        String raw = loadRaw(path);
+        if (StringUtils.hasText(raw)) {
+            return mapper.readValue(raw, HashMap.class);
+        }
+        return Map.of();
+    }
+
+    @SneakyThrows
+    private String loadRaw(String path) {
+        Resource resource = resourceLoader.getResource(ResourceUtils.CLASSPATH_URL_PREFIX + "json" + path);
+        return resource.exists() ? resource.getContentAsString(UTF_8) : "";
+    }
+
+    private String generateAccessToken(User user, Key secretKey, Long tokenExpiration) {
         Instant now = Instant.now();
-        Instant expiration = now.plusMillis(accessTokenExpiration);
-
+        Instant expiration = now.plusMillis(tokenExpiration);
         return Jwts.builder()
-                .setSubject(user.getEmail())
+                .subject(user.getEmail())
                 .claim("role", user.getRole())
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(expiration))
-                .signWith(accessSecret, SignatureAlgorithm.HS512)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(expiration))
+                .signWith(secretKey)
                 .compact();
     }
 
+    private Stream<Arguments> getPositiveTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        Named.of(
+                                "Positive test test case",
+                                EndToEndTestCase.builder()
+                                        .filePathPostfix("/getContestsByName")
+                                        .cookies(Map.of(tokenCookieName, teacherToken))
+                                        .method(Method.GET)
+                                        .path("/contests")
+                                        .statusCode(200)
+                                        .build()
+                        )
+                )
+        );
+    }
+
+    private Stream<Arguments> getNegativeTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        Named.of(
+                                "Negative test test case",
+                                EndToEndTestCase.builder()
+                                        .filePathPostfix("/getContestsByName")
+                                        .cookies(Map.of(tokenCookieName, studentToken))
+                                        .method(Method.GET)
+                                        .path("/contests")
+                                        .statusCode(403)
+                                        .build()
+                        )
+                )
+        );
+    }
+
+    @Builder(toBuilder = true)
     private record EndToEndTestCase(
-            Map<String, Object> headers,
+            String filePathPostfix,
             Map<String, Object> queryParams,
             Map<String, Object> cookies,
             String requestBody,
@@ -163,6 +214,13 @@ public class EndToEndTests {
             String path,
             Integer statusCode,
             String responseBody
+
     ) {
+        static class EndToEndTestCaseBuilder {
+            EndToEndTestCaseBuilder() {
+                filePathPostfix = "";
+                cookies = Map.of();
+            }
+        }
     }
 }
