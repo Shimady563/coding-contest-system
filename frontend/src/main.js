3c27cb133beb63c88b664b58f36b3d513ee1b43f
ChristielBel <kristina04bel@gmail.com>
2025-07-02 21:09:54 +0300

    refactor: remove Vuex from the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import './assets/main.css';
 
 function setupFetchInterceptor() {
@@ -46,5 +45,4 @@ setupFetchInterceptor();
 
 const app = createApp(App);
 app.use(router);
-app.use(store);
 app.mount('#app');
