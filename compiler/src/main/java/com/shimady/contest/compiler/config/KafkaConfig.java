3fe8119784b5b2d50ca3ac293fba7a0c88699a1b
Shimady563 <shimady563@gmail.com>
2025-07-20 16:43:25 +0300

    fix: fix topic creation

--- compiler/src/main/java/com/shimady/contest/compiler/config/KafkaConfig.java
+++ compiler/src/main/java/com/shimady/contest/compiler/config/KafkaConfig.java
@@ -1,14 +1,29 @@
 package com.shimady.contest.compiler.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
+
+    @Value("${kafka.topic.submission}")
+    private String submissionTopic;
+
+    @Bean
+    public NewTopic submissionTopic() {
+        return TopicBuilder
+                .name(submissionTopic)
+                .partitions(4)
+                .replicas(1)
+                .build();
+    }
 
     @Bean
     public DefaultErrorHandler errorHandler() {
--- manager/src/main/java/com/shimady563/contest/manager/config/KafkaConfig.java
+++ manager/src/main/java/com/shimady563/contest/manager/config/KafkaConfig.java
@@ -1,16 +1,21 @@
 package com.shimady563.contest.manager.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${kafka.topic.submission}")
+    private String submissionTopic;
+
     @Bean
     public NewTopic submissionTopic() {
         return TopicBuilder
-                .name("submissionTopic")
+                .name(submissionTopic)
                 .partitions(4)
                 .replicas(1)
                 .build();
