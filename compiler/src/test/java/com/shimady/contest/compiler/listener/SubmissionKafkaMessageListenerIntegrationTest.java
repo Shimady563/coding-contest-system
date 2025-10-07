47e7a96b3a177ca1b400fdbbdb00f5a300faa98c
Shimady563 <shimady563@gmail.com>
2025-10-07 16:16:34 +0300

    fix(test): add missing property to test container tests

--- compiler/src/test/java/com/shimady/contest/compiler/listener/SubmissionKafkaMessageListenerIntegrationTest.java
+++ compiler/src/test/java/com/shimady/contest/compiler/listener/SubmissionKafkaMessageListenerIntegrationTest.java
@@ -28,7 +28,7 @@ import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.after;
 
 @SpringBootTest
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 @Import({TestcontainersConfiguration.class})
 public class SubmissionKafkaMessageListenerIntegrationTest {
 
