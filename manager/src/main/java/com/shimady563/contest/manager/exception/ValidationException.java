6b338aab7fedb00a77ef1e0a760f327b84c05faf
Shimady563 <shimady563@gmail.com>
2025-10-28 19:22:07 +0300

    feat(helm): introduce basic helm templates

---
+++ manager/src/main/java/com/shimady563/contest/manager/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.shimady563.contest.manager.exception;
+
+public class ValidationException extends RuntimeException {
+  public ValidationException(String message) {
+    super(message);
+  }
+}
