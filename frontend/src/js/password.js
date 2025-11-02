cc2796fddc733672cc48882b4b086ce4dc573928
ChristielBel <kristina04bel@gmail.com>
2025-11-02 22:14:56 +0300

    feat: add password validation function

---
+++ frontend/src/js/password.js
@@ -0,0 +1,15 @@
+export function validatePassword(password) {
+    return {
+      hasMinLength: password.length >= 8,
+      hasUpperCase: /[A-Z]/.test(password),
+      hasLowerCase: /[a-z]/.test(password),
+      hasDigit: /\d/.test(password),
+      hasSpecialChar: /[@#$%^&+=!?*]/.test(password),
+      isValid: password.length >= 8 &&
+               /[A-Z]/.test(password) &&
+               /[a-z]/.test(password) &&
+               /\d/.test(password) &&
+               /[@#$%^&+=!?*]/.test(password)
+    };
+  }
+  
